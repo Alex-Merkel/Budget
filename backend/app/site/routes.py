d5be4b3d44ad8b4817ad2a1dc40631bdc00d35be
Alex Merkel <kajen16@gmail.com>
2023-06-30 15:21:55 -0500

    Budget Logic Started, add expense button implemented

--- backend/app/site/routes.py
+++ backend/app/site/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, session
 
 site = Blueprint('site', __name__, template_folder='site_templates')
 
@@ -10,6 +10,26 @@ def index():
 def profile():
     return render_template('profile.html')
 
-@site.route('/budget')
+@site.route('/budget', methods=['GET', 'POST'])
 def budget():
+    if request.method == 'POST':
+        housing_expense = request.form.get('housing')
+        transportation_expense = request.form.get('transportation')
+        groceries_expense = request.form.get('groceries')
+        utilities_expense = request.form.get('utilities')
+        healthcare_expense = request.form.get('healthcare')
+        debt_payments_expense = request.form.get('debt_payments')
+        emergency_fund_expense = request.form.get('emergency_fund')
+        retirement_expense = request.form.get('retirement')
+        vacation_expense = request.form.get('vacation')
+        entertainment_expense = request.form.get('entertainment')
+        dining_out_expense = request.form.get('dining_out')
+        hobbies_expense = request.form.get('hobbies')
+        other_expense = request.form.get('other')
+        
+        
+
+
+
+        session['housing_expense'] = housing_expense
     return render_template('budget.html')
