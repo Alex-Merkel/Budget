02b4944246073fafbcb79ac75dc0bca2131cd1e0
Alex Merkel <kajen16@gmail.com>
2023-07-19 13:03:44 -0500

    db functioning, migration added

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,8 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
 class Expense(db.Model):
-    __tablename__ = 'expense'
+    __tablename__ = 'expenses'
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     expense_list = db.Column(db.JSON, nullable=False)
