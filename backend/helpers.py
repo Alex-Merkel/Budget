6aebeec33354bce2ebfa5c210e9d27a5d81165d6
Alex Merkel <kajen16@gmail.com>
2023-08-25 15:21:39 -0500

    cleaned up helpers.py, working on total expenses and surplus/deficit values

--- backend/helpers.py
+++ backend/helpers.py
@@ -9,18 +9,13 @@ from models import User
 def token_required(our_flask_function):
     @wraps(our_flask_function)
     def decorated(*args, **kwargs):
-        print('Headers:', request.headers)
         token = None
 
         if 'x-access-token' in request.headers:
             token = request.headers['x-access-token'].split(' ')[1]
-            print(token)
         if not token:
-            print('Token is missing')
             return jsonify({"message" : "Token is missing."}), 401
         
-        print('Token:', token)
-
         try:
             current_user_token = User.query.filter_by(token=token).first()
 
@@ -28,7 +23,6 @@ def token_required(our_flask_function):
             owner = User.query.filter_by(token=token).first()
 
             if token != owner.token and secrets.compare_digest(token, owner.token):
-                print('Token is invalid')
                 return jsonify({"message" : "Token is invalid"})
             
         return our_flask_function(current_user_token, *args, **kwargs)
