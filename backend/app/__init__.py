cfde282291abd97891a0e6149effe39d1c89d943
Alex Merkel <kajen16@gmail.com>
2023-09-21 14:05:02 -0500

    Styling finished, color changing S/D added

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -22,6 +22,7 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)
     login_manager.init_app(app)
+    login_manager.login_view = "auth.signin"
     ma.init_app(app)
     migrate = Migrate(app, db)
 
--- backend/app/site/routes.py
+++ backend/app/site/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for, current_app, jsonify
 from models import db, User, Expense
 import requests
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 site = Blueprint('site', __name__, template_folder='site_templates')
 
@@ -10,9 +10,11 @@ def index():
     return render_template('index.html')
 
 @site.route('/profile')
+@login_required
 def profile():
     return render_template('profile.html')
 
 @site.route('/budget')
+@login_required
 def budget():
         return render_template('budget.html')
