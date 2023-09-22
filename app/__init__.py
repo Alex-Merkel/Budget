85fa3ff0e3d7a722648d46214ae6829203192719
Alex Merkel <kajen16@gmail.com>
2023-09-22 01:25:22 -0500

    Changed __init__.py structure

--- app/__init__.py
+++ app/__init__.py
@@ -10,20 +10,18 @@ from flask_cors import CORS
 from helpers import JSONEncoder
 
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
 
-    app.register_blueprint(site)
-    app.register_blueprint(auth)
-    app.register_blueprint(api)
+app = Flask(__name__)
+CORS(app)
 
-    app.json_encoder = JSONEncoder
-    app.config.from_object(Config)
-    db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = "auth.signin"
-    ma.init_app(app)
-    migrate = Migrate(app, db)
+app.register_blueprint(site)
+app.register_blueprint(auth)
+app.register_blueprint(api)
 
-    return app
+app.json_encoder = JSONEncoder
+app.config.from_object(Config)
+db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_view = "auth.signin"
+ma.init_app(app)
+migrate = Migrate(app, db)
